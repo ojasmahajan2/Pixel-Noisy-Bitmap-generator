13f3cfaa44026ddd394b3f4a479ae74238ac9f59
ojasmahajan2 <ojasamahajan.85@gmail.com>
2026-05-28 04:44:25 +0530

    Updated the entire code semantics + design [Refactor image generation into ImageRenderer class]
    Now using unsigned 8 bit integer instead of previous unsigned char and more changes...

--- main.cpp
+++ main.cpp
@@ -1,143 +1,165 @@
-#include <iostream>
 #include <fstream>
+#include <stdexcept>
+#include <iostream>
 #include <random>
 
-/*———————————————————————————————————————————————————————————————————————————*/
+// ————————————————————————————————————————————————————————————————————————————
+const uint32_t WIDTH  = 16;
+const uint32_t HEIGHT = 16;
 
-const unsigned char BMP[18] = {
-    0x42, 0x4D,             // "BM"
-    0x00, 0x00, 0x00, 0x00, // file size = UNKNOWN bytes
-    0x00, 0x00,             // reserved
-    0x00, 0x00,             // reserved
-    0x36, 0x00, 0x00, 0x00, // pixel data offset = 54
-    0x28, 0x00, 0x00, 0x00  // DIB header size = 40
-};
+bool noise = true;
+bool colored = true;
+// ————————————————————————————————————————————————————————————————————————————
 
-int width  = 1;             // width of image in pixels
-int height = 1;             // height of image in pixels
+struct ImageRenderer {
+public: 
+    void run() {
+        std::fstream image(
+            "Generated_Image.bmp",
+            std::ios::binary | std::ios::out
+        );
 
-const unsigned char metadata[28] = {
-    0x01, 0x00,             // color planes = 1
+        if (image.is_open()) {
+            header(image);
+            imageDimensions(image);
+            metaData(image);
 
-    0x18, 0x00,             // 24 bits per pixel
-    0x00, 0x00, 0x00, 0x00, // no compression
-    0x04, 0x00, 0x00, 0x00, // image size = 4 bytes
+            pixelArray(image);
 
-    0x13, 0x0B, 0x00, 0x00, // horizontal resolution
-    0x13, 0x0B, 0x00, 0x00, // vertical resolution
+            std::cout << "[Completed]: ";
+            std::cout << "Image saved!\n" << std::endl;
 
-    0x00, 0x00, 0x00, 0x00, // colors in palette
-    0x00, 0x00, 0x00, 0x00  // important colors
-};
-
-/*———————————————————————————————————————————————————————————————————————————*/
-struct Pixel {
-    const unsigned char RGB_MAX = 255;
-    const unsigned char RGB_MIN = 0;
-
-    unsigned char R = RGB_MAX;  // default white
-    unsigned char G = RGB_MAX;  // default white
-    unsigned char B = RGB_MAX;  // default white
-};
-/*———————————————————————————————————————————————————————————————————————————*/
-
-void effect(Pixel &color, bool useNoise, bool Color, int &x, int &y) {
-    float horizontal = x / float(width );   // Gradient factor width
-    float vertical   = y / float(height);   // Gradient factor height
-
-    if (!useNoise) {
-        color.R = (color.RGB_MAX * horizontal * (1 - vertical));
-        color.G = (color.RGB_MAX * horizontal * vertical);
-        color.B = (color.RGB_MAX * (1 - horizontal) * vertical);
-
-        return;
-    }
-
-    static std::random_device rd;
-    static std::mt19937 rgb(rd());
-
-    static std::uniform_int_distribution<> 
-                range(color.RGB_MIN, color.RGB_MAX);
-
-    if (Color) {
-        color.R = range(rgb);
-        color.G = range(rgb);
-        color.B = range(rgb);
-    } else {
-        color.R = color.G = color.B = range(rgb);
-    }
-}
-
-void generateImage(bool &isNoise, bool &isColorNoise) {
-    Pixel px;
-
-    std::fstream image(
-        "Generated_Image.bmp",
-        std::ios::binary | std::ios::out
-    );
-    if (image.is_open()) {
-        for (int i = 0; i < 18; i++) {
-            image.put(BMP[i]);
-        }
-
-        image.write(reinterpret_cast<char*>(&width), sizeof(width));
-        image.write(reinterpret_cast<char*>(&height), sizeof(height));
-
-        for (int i = 0; i < 28; i++) {
-            image.put(metadata[i]);
-        }
-
-        for (int y = 1; y <= height; y++) {
-            for (int x = 1; x <= width; x++) {
-                effect(px, isNoise, isColorNoise, x, y);
-                
-                image.put(px.B);
-                image.put(px.G);
-                image.put(px.R);
-            }
-
-            int progress = (y * 100) / height;
-            std::cout << "Bitmap Generating... " << progress
-                << "%" << "\r" << std::flush;
-            if (progress == 100) std::cout << "\n" << std::endl;
-
-            const int shiftRow = (4 - (width * 3) % 4) % 4;     
-            for (int k = 1; k <= shiftRow; k++) {
-                image.put(0);
-            }
-        }
-
-        if (isNoise) {
-            std::cout << "[NOISY]";
-            if (isColorNoise) {
-                std::cout << "[COLORED]";
-            }
-            std::cout << ": Generated_Image.bmp" << std::endl;
+            image.close();
         } else {
-            std::cout << "[GRADIENT]: Generated_Image.bmp" << std::endl;
+            throw std::runtime_error("Failed to open the file!");
         }
-        std::cout << "Saved generated image!" << std::endl;
-
-        image.close();
-    } else {
-        std::cerr << "[ERROR]: Failed to create/generate image"
-            << std::endl;
     }
-}
 
-/*———————————————————————————————————————————————————————————————————————————*/
+private:
+    void writeBinary(std::fstream& img, const auto& value) {
+        img.write(reinterpret_cast<const char*>(&value), sizeof(value));
+    }
+
+// ============================================================================
+    #pragma pack(push, 1)
+    struct BitmapHeader {
+        const uint32_t FileSize      
+            = 14 + 40 + (WIDTH * 3 + (4 - (WIDTH * 3) % 4) % 4) * HEIGHT;
+
+        const uint32_t Reserved      = 0;
+        const uint32_t PxDataOffset  = 14 + 40;
+        const uint32_t DIBHeaderSize = 40;
+    };
+    #pragma pack(pop)
+
+    #pragma pack(push, 1)
+    struct DIBmetadata {
+        const uint16_t ColorPlanes          = 1;
+        const uint16_t BitsPerPixel         = 24;
+        const uint32_t Compression          = 0;
+        const uint32_t ImageSize            = 0;
+        const uint32_t HorizontalResolution = 2835;
+        const uint32_t VerticalResolution   = 2835;
+        const uint32_t PaletteColors        = 0;
+        const uint32_t ImportantColors      = 0;
+    };
+    #pragma pack(pop)
+// ============================================================================
+
+    void header(std::fstream& img) {
+        const char* Signature        = "BM";
+        img.write(Signature, 2);
+
+        BitmapHeader bmpHeader;
+        writeBinary(img, bmpHeader);
+    }
+
+    void imageDimensions(std::fstream& img) {
+        writeBinary(img, WIDTH);
+        writeBinary(img, HEIGHT);
+    }
+
+    void metaData(std::fstream& img) {
+        DIBmetadata mtadta;
+        writeBinary(img, mtadta);
+    }
+
+// ============================================================================
+    struct PixelRGB {
+        uint8_t B = 255;
+        uint8_t G = 255;
+        uint8_t R = 255;
+    };
+// ============================================================================
+    
+    void pixelArray(std::fstream& img) {
+        PixelRGB pixel;
+        const uint8_t ShiftRow = (4 - (WIDTH * 3) % 4) % 4;
+
+        for (uint32_t column = 0; column < HEIGHT; column++) {
+            for (uint32_t row = 0; row < WIDTH; row++) {
+                effect(pixel, row, column);
+                writeBinary(img, pixel);
+            }
+
+            const char padding[3] = {0, 0, 0};
+            img.write(padding, ShiftRow);
+
+            std::cout << "Rendering: " << ((column + 1) * 100) / HEIGHT << "%"
+                        << "\r" << std::flush;
+        }
+
+        std::cout << "\n";
+    }
+
+    void effect(PixelRGB& color, const uint32_t& x, const uint32_t& y) {
+        const uint8_t RGB_MAX = 255;
+        const uint8_t RGB_MIN = 0;
+
+        if (!noise && colored) {
+            float horizontal = x / float(WIDTH);    // Gradient factor width
+            float vertical   = y / float(HEIGHT);   // Gradient factor height
+
+            color.R = (RGB_MAX * horizontal * (1 - vertical));
+            color.G = (RGB_MAX * horizontal * vertical);
+            color.B = (RGB_MAX * (1 - horizontal) * vertical);
+
+            return;
+        }
+        else if (!noise && !colored) {
+            return;
+        }
+
+        static std::random_device rd;
+        static std::mt19937 rgb(rd());
+
+        static std::uniform_int_distribution<> 
+                    range(RGB_MIN, RGB_MAX);
+
+        if (noise && colored) {
+            color.R = range(rgb);
+            color.G = range(rgb);
+            color.B = range(rgb);
+        } else {
+            color.R = color.G = color.B = range(rgb);
+        }
+    }
+};
+
 int main() {
     std::cout << "\033[2J\033[1;1H" << std::flush;
     std::cout << "\033[?25l" << std::flush;
 
-    /*———————————————————————————————————————————————————————————————————————*/
-    bool noise = true;
-    bool colored = true;
+    try {
+        ImageRenderer app;
+        app.run();
+    }
+    catch (const std::exception& error) {
+        std::cerr << error.what() << "\n";
+        return EXIT_FAILURE;
+    }
 
-    width  = 16;
-    height = 16;
-    /*———————————————————————————————————————————————————————————————————————*/
-    generateImage(noise, colored);
+    std::cout << "\033[?25h" << std::flush;
 
-    return 0;
+    return EXIT_SUCCESS;
 }
